227d47a9e633de806e07ea91831119fcfae1b118
Joel Sandén <me@joelsanden.se>
2024-08-11 10:57:57 +0200

    converted to turborepo

---
+++ prettier.config.mjs
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url';
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+
+/** @type { PrettierConfig | TailwindConfig } */
+export default {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-packagejson'],
+  tailwindConfig: fileURLToPath(
+    new URL('./apps/qualia/tailwind.config.ts', import.meta.url),
+  ),
+  tailwindFunctions: ['cn', 'cva'],
+};
